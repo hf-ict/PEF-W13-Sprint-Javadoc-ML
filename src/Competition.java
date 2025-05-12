916db04e1f7d121a97bb5a7b5d60f7b2b56fcef1
Roger Zaugg <roger.zaugg@sbl.ch>
2025-05-12 15:40:15 +0200

    change structure to demonstrate static methods

--- src/Competition.java
+++ src/Competition.java
@@ -10,27 +10,33 @@ public class Competition {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Athlete a1 = new Athlete("Coleman");
-		Athlete a2 = new Athlete("Gatlin");
-		Athlete a3 = new Athlete("De Grasse");
-		Athlete a4 = new Athlete("Simbine");
-		Athlete a5 = new Athlete("Blake");
-		Athlete a6 = new Athlete("Hughes");
-		Athlete a7 = new Athlete("Tortu");
-		Athlete a8 = new Athlete("Brown");
-
 		Sprint sprint = new Sprint();
+
+		Athlete a1 = new Athlete("Coleman");
 		sprint.addAthlete(a1);
+
+		Athlete a2 = new Athlete("Gatlin");
 		sprint.addAthlete(a2);
+
+		Athlete a3 = new Athlete("De Grasse");
 		sprint.addAthlete(a3);
+
+		Athlete a4 = new Athlete("Simbine");
 		sprint.addAthlete(a4);
+
+		Athlete a5 = new Athlete("Blake");
 		sprint.addAthlete(a5);
+
+		Athlete a6 = new Athlete("Hughes");
 		sprint.addAthlete(a6);
+
+		Athlete a7 = new Athlete("Tortu");
 		sprint.addAthlete(a7);
+
+		Athlete a8 = new Athlete("Brown");
 		sprint.addAthlete(a8);
-		
+
 		sprint.start();
 		sprint.print();
 	}
-
 }
